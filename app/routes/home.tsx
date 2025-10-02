59747ec9c7e6e7a9922c61d45cae7bd60ddfe94a
Lawrence496 <lawrenceanyalechi@gmail.com>
2025-10-02 17:07:09 +0100

    Add Navbar component and update Home route; remove unused logo files and Welcome component

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,21 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart Feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-no-repeat bg-cover min-h-screen">
+    <Navbar />
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Welcome to Resumind</h1>
+        <p>Your AI-powered resume assistant</p>
+      </div>
+    </section>
+  </main>
 }
