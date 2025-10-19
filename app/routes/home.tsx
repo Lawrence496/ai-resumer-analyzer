066840af2c9ca79a834ccaf4fb8920c4ca705847
Lawrence <lawrenceanyalechi@gmail.com>
2025-10-19 20:02:47 +0100

    Add Puter integration with Zustand store; implement authentication and file system interfaces

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,9 @@ import ResumeCard from "~/components/ResumeCard";
 import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar";
 import { resumes } from "../../constants";
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/puter"
 
 
 export function meta({}: Route.MetaArgs) {
@@ -12,10 +15,20 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+    const { isLoading, auth } = usePuterStore()
+
+    const location = useLocation();
+    const next = location.search.split('next=')[1];
+    const navigate = useNavigate();
+
+    useEffect(() => {
+      if(!auth.isAuthenticated) navigate('auth?next=/');
+    }, [auth.isAuthenticated]);
+
   return <main className="bg-[url('/images/bg-main.svg')] bg-no-repeat bg-cover min-h-screen">
     <Navbar />
     <section className="main-section">
-      <div className="page-heading">
+      <div className="page-heading py-16">
         <h1>Welcome to Resumind</h1>
         <p>Your AI-powered resume assistant</p>
       </div>
